25838d75d3c799af61b69bd79e7fe96f46290aba
Alexey Zvezdin <alexeyrush94@gmail.com>
2019-06-11 19:01:49 +0400

    Added graphql schema

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 function createStore(reducer, initialState) {
   let state = initialState;
@@ -21,30 +21,29 @@ function createStore(reducer, initialState) {
     getState,
     dispatch
   };
-
 }
 
 // Need to use singleton to fetch space X api data if not fetched yet
-// To fetched data need to be added a boolean variable to know if we can 
-// delete already existing launch 
+// To fetched data need to be added a boolean variable to know if we can
+// delete already existing launch
 // end of fetching space X data
 
-
-function addMissionReducer = (state, action) {
+function addMissionReducer(state, action) {
   if (action.type === "ADD_MISSION") {
     return {
       // later will be added boolean to know if data of existing
       // launch can be deleted
-      missions: state.missions.concat(action.mission);
-    }
+      missions: state.missions.concat(action.mission)
+    };
   } else if (action.type === "DELETE_MISSION") {
     return {
-      missions: [...state.missions.slice(0, action.mission_id),
+      missions: [
+        ...state.missions.slice(0, action.mission_id),
         ...state.missions.slice(action.mission_id + 1, state.missions.length)
       ]
-    } 
-  } 
-  # @EDIT_MISSION_DATA will be added later due to complexity
+    };
+  }
+  // # @EDIT_MISSION_DATA will be added later due to complexity
   // else if (action.type === "EDIT_MISSION_DATA") {
   //   return {
 
@@ -52,24 +51,22 @@ function addMissionReducer = (state, action) {
   // }
 }
 
-class MessageContainer extends Component {
-  
-  render() {
-    return (
+// will be fetched through graphql queries.
+const initialState = [];
 
-    )
+class MissionsContainer extends Component {
+  render() {
+    return <h1>Halo</h1>;
   }
 }
-
-
-
 
 export default class App extends Component {
   render() {
     return (
       <div>
-          <h1>Hella fellows</h1>
+        <MissionsContainer />
+        <h1>Hella fellows</h1>
       </div>
-    )
+    );
   }
 }
--- schema.js
+++ schema.js
@@ -7,3 +7,76 @@ const {
   GraphQLList,
   GraphQLSchema
 } = require("graphql");
+
+// Launch type
+
+const LaunchType = new GraphQLObjectType({
+  name: "Launch",
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType },
+    launch_site: { type: LaunchSiteType },
+    launch_failure_details: { type: LaunchFailDetails },
+    links: { type: LinksType },
+    details: { type: GraphQLString }
+  })
+});
+
+const LaunchSiteType = new GraphQLObjectType({
+  name: "Launch Site Type",
+  fields: () => ({
+    site_name_long: { type: GraphQLString }
+  })
+});
+
+const LaunchFailureDetails = new GraphQLObjectType({
+  name: "Launch Failure Details",
+  fileds: () => ({
+    time: { type: GraphQLInt },
+    altitude: { type: GraphQLInt },
+    reason: { type: GraphQLString }
+  })
+});
+
+const Link = new GraphQLObjectType({
+  name: "Links",
+  fields: () => ({
+    mission_patch_small: { type: GraphQLString },
+    video_link: { type: GraphQLString }
+  })
+});
+
+// Rocket Type
+
+const RocketType = new GraphQLObjectType({
+  name: "Rocket",
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString },
+    first_stage: { type: FirstStage },
+    second_stage: { type: SecondStage }
+  })
+});
+
+// Rocket First Stage
+
+const FirstStage = new GraphQLObjectType({
+  name: "First Stage",
+  fields: () => ({
+    cores: [core]
+  })
+});
+
+//  Rocket Second Stage
+
+const SecondStage = new GraphQLObjectType({
+  name: "Second Stage",
+  fields: () => ({
+    payloads: [payload]
+  })
+});
